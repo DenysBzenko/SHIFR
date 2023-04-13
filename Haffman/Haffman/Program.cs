72169eb02b688de7e8ee009067b7460c5ee100b1
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-04-13 22:54:32 +0300

    Підрахунок частоти символів у тексті

---
+++ Haffman/Haffman/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Text;
+
+class Node
+{
+    public char Symbol;
+    public int Frequency;
+    public Node Left;
+    public Node Right;
+}
+
+class Program
+{
+    // Підрахунок частоти символів у тексті
+    static Dictionary<char, int> CountSymbols(string text)
+    {
+        var result = new Dictionary<char, int>();
+        foreach (var symbol in text)
+        {
+            if (result.ContainsKey(symbol))
+                result[symbol]++;
+            else
+                result[symbol] = 1;
+        }
+        return result;
